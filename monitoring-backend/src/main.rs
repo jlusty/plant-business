bdce9e3211c25226f8421fd89bc3b34ab34cd514
John Lusty <54030459+jlusty@users.noreply.github.com>
2021-05-01 23:17:17 +0100

    Move data models to separate folder

--- monitoring-backend/src/main.rs
+++ monitoring-backend/src/main.rs
@@ -7,69 +7,20 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
 
-// Exposes schema file to be available here
-pub mod schema;
+// Exposes other files to be available
+mod models;
+mod schema;
 
-use chrono::{DateTime, NaiveDateTime, Utc};
+use crate::models::{PlantMetricEntity, PlantMetricInsert};
+use chrono::DateTime;
 use diesel::prelude::*;
-use rocket::{fairing::AdHoc, response::status::Created, response::Debug, Build, Rocket};
+use rocket::{
+    fairing::AdHoc,
+    response::{status::Created, Debug},
+    Build, Rocket,
+};
 use rocket_contrib::{databases::diesel as rocket_diesel, json::Json};
 use schema::plant_metrics; // Used to get db schema
-use serde::{Deserialize, Serialize};
-
-// Model type used to interact with DBO
-// Queryable: Allows diesel to get this type back when querying the DB
-// Serialize: Allows serde to serialize the database entry to JSON of this type
-#[derive(Queryable, Serialize)]
-pub struct PlantMetricEntity {
-    #[serde(with = "my_date_format")]
-    pub recorded_at: NaiveDateTime,
-    pub temperature: Option<f32>,
-    pub humidity: Option<f32>,
-    pub light: Option<i32>,
-    pub soil_moisture: Option<i32>,
-}
-
-mod my_date_format {
-    use chrono::{DateTime, NaiveDateTime, Utc};
-    use serde::{Deserialize, Deserializer, Serializer};
-
-    pub fn serialize<S: Serializer>(
-        time: &NaiveDateTime,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error> {
-        let s = DateTime::<Utc>::from_utc(*time, Utc).to_rfc3339();
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D: Deserializer<'de>>(
-        deserializer: D,
-    ) -> Result<NaiveDateTime, D::Error> {
-        let time: String = Deserialize::deserialize(deserializer)?;
-        Ok(DateTime::parse_from_rfc3339(&time)
-            .map_err(serde::de::Error::custom)?
-            .naive_utc())
-    }
-}
-
-fn time_now() -> NaiveDateTime {
-    Utc::now().naive_utc()
-}
-
-// Insertable: Allows diesel to use this type when inserting into the DB
-// Deserialize: Allows serde to deserialize request body to this type
-#[derive(Clone, Insertable, Deserialize)]
-#[table_name = "plant_metrics"] // Table where to insert
-#[serde(deny_unknown_fields)] // Fails if extra fields provided in JSON body
-pub struct PlantMetricInsert {
-    // When deserialising, add the time now as the recorded at time
-    #[serde(skip_deserializing, default = "time_now")]
-    pub recorded_at: NaiveDateTime,
-    pub temperature: f32,
-    pub humidity: f32,
-    pub light: i32,
-    pub soil_moisture: i32,
-}
 
 // Used to connect Rocket to the PostgreSQL database
 #[database("postgres_timeseries")]
@@ -104,13 +55,11 @@ pub fn stage() -> AdHoc {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(stage()).mount("/", routes![index])
+    rocket::build().attach(stage()).mount("/", routes![health])
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+#[get("/health")]
+fn health() {}
 
 #[get("/metric/<time_str>")]
 async fn get_timeseries(
---
+++ monitoring-backend/src/models.rs
@@ -0,0 +1,57 @@
+use super::schema::plant_metrics; // Used to get db schema
+use chrono::{NaiveDateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+// Model type used to interact with DBO
+// Queryable: Allows diesel to get this type back when querying the DB
+// Serialize: Allows serde to serialize the database entry to JSON of this type
+#[derive(Queryable, Serialize)]
+pub struct PlantMetricEntity {
+    #[serde(with = "my_date_format")]
+    pub recorded_at: NaiveDateTime,
+    pub temperature: Option<f32>,
+    pub humidity: Option<f32>,
+    pub light: Option<i32>,
+    pub soil_moisture: Option<i32>,
+}
+
+// Insertable: Allows diesel to use this type when inserting into the DB
+// Deserialize: Allows serde to deserialize request body to this type
+#[derive(Clone, Insertable, Deserialize)]
+#[table_name = "plant_metrics"] // Table where to insert
+#[serde(deny_unknown_fields)] // Fails if extra fields provided in JSON body
+pub struct PlantMetricInsert {
+    // When deserialising, add the time now as the recorded at time
+    #[serde(skip_deserializing, default = "time_now")]
+    pub recorded_at: NaiveDateTime,
+    pub temperature: f32,
+    pub humidity: f32,
+    pub light: i32,
+    pub soil_moisture: i32,
+}
+
+mod my_date_format {
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S: Serializer>(
+        time: &NaiveDateTime,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        let s = DateTime::<Utc>::from_utc(*time, Utc).to_rfc3339();
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<NaiveDateTime, D::Error> {
+        let time: String = Deserialize::deserialize(deserializer)?;
+        Ok(DateTime::parse_from_rfc3339(&time)
+            .map_err(serde::de::Error::custom)?
+            .naive_utc())
+    }
+}
+
+fn time_now() -> NaiveDateTime {
+    Utc::now().naive_utc()
+}
