4302c957e36e7adbc7b52663d736aac5b7720d57
John Lusty <54030459+jlusty@users.noreply.github.com>
2021-05-02 19:48:18 +0100

    Working chart

--- charts-frontend/src/Chart.svelte
+++ charts-frontend/src/Chart.svelte
@@ -1,46 +1,131 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import {
-    Chart,
-    LineElement,
-    LineController,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    Tooltip,
-  } from "chart.js";
+  // import {
+  //   Chart,
+  //   LineElement,
+  //   LineController,
+  //   CategoryScale,
+  //   LinearScale,
+  //   TimeScale,
+  //   PointElement,
+  //   Tooltip,
+  // } from "chart.js";
 
-  Chart.register(
-    LineElement,
-    LineController,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    Tooltip
-  );
+  // Chart.register(
+  //   LineElement,
+  //   LineController,
+  //   CategoryScale,
+  //   LinearScale,
+  //   TimeScale,
+  //   PointElement,
+  //   Tooltip
+  // );
+  import Chart from "chart.js/auto";
+
   Chart.overrides.line.spanGaps = true;
 
   let canvasElement: HTMLCanvasElement;
+
+  const tempData = {
+    temperature: [
+      {
+        time: "2021-05-02T13:08:26.684215+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-02T01:06:30.774057+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:46:29.589740+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:45:23.835608+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:35:51.283+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:34:33.953324+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:34:31.952755+00:00",
+        data: 35,
+      },
+      {
+        time: "2021-05-01T23:30:49.669813+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:30:48.215167+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:30:46.877377+00:00",
+        data: 0.1,
+      },
+      {
+        time: "2021-05-01T23:30:43.490076+00:00",
+        data: 0.1,
+      },
+    ],
+  };
+
+  const humidData = {
+    humidity: [
+      {
+        time: "2021-05-02T06:30:46.877377+00:00",
+        data: 25,
+      },
+      {
+        time: "2021-05-01T23:30:43.490076+00:00",
+        data: 0.1,
+      },
+    ],
+  };
+
+  import { enGB } from "date-fns/locale";
+  import "chartjs-adapter-date-fns";
 
   onMount(() => {
     new Chart(canvasElement, {
       type: "line",
       data: {
-        labels: [0, 1, 2, 3, 4, 5, 6, 7],
         datasets: [
           {
             label: "Temperature",
-            data: [65, 59, 80, 81, 31, 85, 40],
+            data: tempData.temperature,
             fill: false,
             borderColor: "rgb(75, 192, 192)",
           },
           {
             label: "Humidity",
-            data: [65, 59, 30, 81, null, 55, 40],
+            data: humidData.humidity,
             fill: false,
             borderColor: "rgb(0, 0, 192)",
           },
         ],
+      },
+      options: {
+        parsing: {
+          xAxisKey: "time",
+          yAxisKey: "data",
+        },
+        scales: {
+          x: {
+            type: "time",
+            adapters: { date: { locale: enGB } },
+            // time: {
+            //   displayFormats: {
+            //     quarter: "MMM YYYY",
+            //   },
+            // },
+          },
+        },
       },
     });
   });
